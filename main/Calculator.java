7600b96638274bcea6111ddbf833bc19a761c011
Pooh555 <peerasit.lu@gmail.com>
2025-01-22 21:23:17 +0700

    Add comments

--- main/Calculator.java
+++ main/Calculator.java
@@ -18,12 +18,9 @@ public class Calculator {
     public static void setValues(double[] cameraPosition, double[] lookedPosition, double x, double y, double z) {
         Vector viewToPoint = new Vector(x - cameraPosition[0], y - cameraPosition[1], z - cameraPosition[2]); // vector from the camera to the 3D point to project
         Vector viewVector = new Vector(lookedPosition[0] - cameraPosition[0], lookedPosition[1] - cameraPosition[1], lookedPosition[2] - cameraPosition[2]); // vector from the camera to the looked position
-        // Vector directionVector = new Vector(1, 1, 1); // for projecting onto a plane
-        // Vector planeVector1 = viewVector.crossProduct(directionVector); // first plane vector
-        // Vector planeVector2 = viewVector.crossProduct(planeVector1); // second plane vector
-        Vector rotationVector = getRotationVector(cameraPosition, lookedPosition); // screen rotation compensation
-        Vector abstractVector1 = viewVector.crossProduct(rotationVector);
-        Vector abstractVector2 = viewVector.crossProduct(abstractVector1);
+        Vector rotationVector = getRotationVector(cameraPosition, lookedPosition); // vector that compensates for the camera's orientation
+        Vector abstractVector1 = viewVector.crossProduct(rotationVector); // vector that is perpendicular to both view vector and rotation vector and lies in the screen plane
+        Vector abstractVector2 = viewVector.crossProduct(abstractVector1); // perpendicular to viewVector and orthogonal to abstractVector1
 
         // computing projections point
         double t = ((viewVector.x * lookedPosition[0] + viewVector.y * lookedPosition[1] + viewVector.z * lookedPosition[2])
@@ -40,15 +37,17 @@ public class Calculator {
          * t < 0: the point is behind the camera
          */        
         if (t > 0) {
+            // placing places the 3D point in the 2D screen space relative to the camera's perspective
             drawX = abstractVector2.x * x + abstractVector2.y * y + abstractVector2.z * z;
             drawY = abstractVector1.x * x + abstractVector1.y * y + abstractVector1.z * z;
         }
     }
 
     public static Vector getRotationVector(double[] cameraPosition, double[] lookedPosition) {
+        // relative x and y distances (dx and dy) between the camera position and the point the camera is looking at.
         double dx = Math.abs(cameraPosition[0] - lookedPosition[0]);
         double dy = Math.abs(cameraPosition[1] - lookedPosition[1]);
-        double xRotation = 0, yRotation = 0;
+        double xRotation, yRotation;
         xRotation = dy / (dx + dy);
         yRotation = dx / (dx + dy);
 
